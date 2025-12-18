15dec0389652f880331654bc3fac8eaa9a7fbc8a
asif <asiftanvir2006@gmail.com>
2025-12-18 23:57:38 +0530

    fixed block physics

--- latest.py
+++ latest.py
@@ -1,8 +1,6 @@
 import pygame
-# import clo
 
 pygame.init()
-# 400 is the default level of floor where everything spawns
 
 #############################################
 # Global Constants & Screen Setup
@@ -91,12 +89,16 @@ class immovable_objects:
 
             # Movable object collides vertically
             elif not self.check_collision_y(obj):
-                return 0  # Ready for conversion to immovable
-
+                if(obj.hitbox.centerx<self.hitbox.centerx):
+                    obj.can_move_right = 0
+                elif(obj.hitbox.centerx>self.hitbox.centerx):
+                    obj.can_move_left = 0
+            return 0
 
 #############################################
 # MOVABLE OBJECTS (BOXES)
 #############################################
+
 class movable_objects:
 
     def __init__(self, x, y, image, width=1, height=1):
@@ -107,19 +109,17 @@ class movable_objects:
         # Load and scale image
         self.image = pygame.image.load(image)
         self.image = pygame.transform.scale(self.image, (width, height))
-
+        self.can_move_right = 1
+        self.can_move_left = 1
+        
         # Create hitbox
         self.hitbox = self.image.get_rect()
         self.hitbox.centerx = self.x
         self.hitbox.bottom = self.y
 
         # Physics properties
-        self.velocity = 4
-        self.movingLeft = 0
         self.floor = 400
         self.roof = -INF
-        self.leftWall = 0
-        self.rightWall = SCREEN_WIDTH
         self.fall_counter = 0
 
     def draw(self):
@@ -127,7 +127,6 @@ class movable_objects:
         self.hitbox.bottom = self.y
         SCREEN.blit(self.image, self.hitbox)
 
-        
 
     def check_collision_x(self, obj):
         # If obj is left return -1
@@ -167,24 +166,27 @@ class movable_objects:
 
             # Object is colliding vertically
             elif not self.check_collision_y(obj):
+                if(not self.can_move_right and self.hitbox.centerx>obj.hitbox.centerx):
+                    obj.can_move_right = self.can_move_right
+                if(not self.can_move_left and self.hitbox.centerx<obj.hitbox.centerx):
+                    obj.can_move_left = self.can_move_left
 
-                # Player pushing from left
-                if obj.hitbox.centerx < self.hitbox.centerx and player_obj.hitbox.centerx < self.hitbox.centerx:
-                    print("aaaaaa")
-                    self.movingLeft = 0
+                # Player pushing right
+
+                if obj.hitbox.centerx < self.hitbox.centerx and player_obj.hitbox.centerx < self.hitbox.centerx and self.can_move_right:
+                    self.can_move_left=1
                     self.hitbox.left = obj.hitbox.right
                     self.x = self.hitbox.centerx
-                    pass
+                    
 
-                # Player pushing from right
-                elif obj.hitbox.centerx > self.hitbox.centerx and player_obj.hitbox.centerx > self.hitbox.centerx:
-                    print("dddddddd")
-                    self.movingLeft = 1
+                # Player pushing left
+                elif obj.hitbox.centerx > self.hitbox.centerx and player_obj.hitbox.centerx > self.hitbox.centerx and self.can_move_left:
+                    self.can_move_right = 1
+                    print(self.can_move_right)
+                    print("alpha")
                     self.hitbox.right = obj.hitbox.left
                     self.x = self.hitbox.centerx
 
-                    # obj.rightWall = self.hitbox.left
-                    # obj.velocity = 0
 
 
 #############################################
@@ -200,7 +202,8 @@ class player:
         self.hitbox = self.image.get_rect()
         self.hitbox.centerx = self.x
         self.hitbox.bottom = self.y
-
+        self.can_move_right = 1
+        self.can_move_left = 1
         # Movement state
         self.in_motion = 1
         self.movingLeft = 0
@@ -226,12 +229,16 @@ class player:
         pygame.draw.rect(SCREEN, (255, 0, 0), self.hitbox, 2)
 
     def moveLeft(self):
+        if(not self.can_move_left):
+            return
         if self.hitbox.left - self.velocity >= self.leftWall:
             self.x -= self.velocity
         else:
             self.x = self.leftWall + self.hitbox.width / 2
 
     def moveRight(self):
+        if(not self.can_move_right):
+            return
         if self.hitbox.right + self.velocity <= self.rightWall:
             self.x += self.velocity
         else:
@@ -288,17 +295,26 @@ class ground:
     def draw(self):
         SCREEN.blit(self.image, self.hitbox)
 
+class sky:
+    def __init__(self, image, width=1920, height=1080):
+        self.image = pygame.image.load(image)
+        self.image = pygame.transform.scale(self.image, (width, height))
+        self.hitbox = self.image.get_rect()
+        self.hitbox.top = 0
+        self.hitbox.left = 0
 
+    def draw(self):
+        SCREEN.blit(self.image, self.hitbox)
 #############################################
 # WRAPPER CLASS (HOLDS ALL OBJECTS)
 #############################################
 class wrapper_objects:
     def __init__(self):
         self.movable_objects_list = [
-            movable_objects(200, 0, "./assets/image3.png", 90, 150),
-            movable_objects(300, 0, "./assets/image3.png", 90, 300),
-            movable_objects(400, 0, "./assets/image3.png", 90, 450),
-            movable_objects(600, 0, "./assets/image3.png", 90, 60),
+            movable_objects(1000, 0, "./assets/image3.png", 90, 150),
+            movable_objects(1200, 0, "./assets/image3.png", 90, 300),
+            movable_objects(1400, 0, "./assets/image3.png", 90, 450),
+            movable_objects(1600, 0, "./assets/image3.png", 90, 60),
         ]
 
         # First two immovables define screen bounds
@@ -310,7 +326,7 @@ class wrapper_objects:
 
         self.ground_obj = ground(800, "./assets/ground.jpeg")
         self.player_obj = player(100, 400, "./assets/player.png")
-
+        self.skybox = sky("./assets/sky.jpg")
 
 #############################################
 # MAIN LOOP HELPERS
@@ -332,21 +348,23 @@ def reinitalise_params():
     objects.player_obj.leftWall = 0
     objects.player_obj.velocity = 4
     objects.player_obj.rightWall = SCREEN_WIDTH
-
+    objects.player_obj.can_move_right = 1
+    objects.player_obj.can_move_left = 1
+    
     for mov in objects.movable_objects_list:
         mov.floor = 800
         mov.leftWall = -INF
         mov.rightWall = INF
-        mov.in_motion = 0
 
 def draw_all():
+    objects.skybox.draw()
+
     for box in objects.movable_objects_list:
         box.draw()
     for platform in objects.immovable_objects_list:
         platform.draw()
     objects.player_obj.draw()
     objects.ground_obj.draw()
-
 def update_player_wrt_immovable():
     for immovable_object in objects.immovable_objects_list:
         immovable_object.update(objects.player_obj)
@@ -409,6 +427,10 @@ def handle_key_presses():
                 objects.player_obj.movingLeft = 0
             elif event.key == pygame.K_SPACE:
                 objects.player_obj.jumping = 1
+            elif event.key == pygame.K_w:
+                objects.player_obj.y -= 700
+            elif event.key == pygame.K_s:
+                objects.player_obj.y += 135
 
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_a:
@@ -434,9 +456,10 @@ if __name__ == "__main__":
         reinitalise_params()
 
         update_player_wrt_immovable()
+        update_movables_wrt_immovables()
+
         update_movables_wrt_player()
         update_movables_wrt_other_movables()
-        update_movables_wrt_immovables()
 
         check_fall_movables()
         update_player()
